825324cf1eaab772762a24969bfdfc6805e0218c
coder785 <116183840+coder785@users.noreply.github.com>
2023-07-04 21:50:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-// complete the given function
-
 function palindrome(str){
-let ans=str.reverse();
-	if(str===ans){
-		return true;
-	}else{
-		return false;
+  let s="";
+	let strs=str.toLowerCase();
+	for(let i=0;i<strs.length;i++){
+		  if(strs[i]!=' '){
+			  s=s+strs[i];
+		  }
+	} let i=0; 
+	let r=s.length-1;
+	while(i<r){
+		if(s[i]!==s[r]){
+			return false;
+		}
+		i++; 
+		r--;
 	}
+return true;
+	
 }
 module.exports = palindrome
